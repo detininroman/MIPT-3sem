5cd061a2c36f5e6afffaa5449c8eb78bda7c6d07
Roman Detinin <detininroman@gmail.comgit config --global user.name Roman>
2017-09-24 20:34:13 +0300

    fifos v1.0

---
+++ sem3/task2 [fifo]/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <fcntl.h>
+
+const char* pipe_name = "/tmp/pipe";
+enum { PIPE_SIZE = 16384 };
+
+int send    (int input);
+int recieve (int output);
+
+int main (int argc, char* argv[])
+{
+	if (argc == 1)
+	{	
+		return recieve (STDOUT_FILENO);
+	}
+
+	else if (argc == 2) 
+	{
+		int input = open (argv[1], O_RDONLY);
+	
+		if (input == -1) {
+			perror ("Can't open the file!\n");
+			exit (EXIT_FAILURE);
+		}
+
+		return send (input);
+	}
+	
+	perror ("Incorrect arguments!\n");
+	return 0;
+}
+
+int send (int input)
+{
+	char buf [PIPE_SIZE] = {};
+	ssize_t file_size = 0;
+	
+	mkfifo (pipe_name, 0666);
+
+	int pipe = open (pipe_name, O_WRONLY);
+	if (pipe == -1) {
+		perror ("Can't open the pipe!\n");
+		exit (EXIT_FAILURE);
+	}
+	
+	while ((file_size = read (input, buf, PIPE_SIZE)) > 0) {
+		write (pipe, buf, file_size);
+	}
+
+	close (input);
+	close (pipe);
+
+	return 0;
+}
+
+int recieve (int output)
+{
+	char buf [PIPE_SIZE] = {};
+	int file_size = 0;
+
+        int pipe = open (pipe_name, O_RDONLY);
+	if (pipe == -1)  {
+		perror ("Can't open the pipe!\n");
+		exit (EXIT_FAILURE);
+	}
+
+	while ((file_size = read (pipe, buf, PIPE_SIZE)) > 0)
+		write (output, buf, file_size);
+	
+	close (pipe);
+	close (output);
+
+	return 0;
+}
