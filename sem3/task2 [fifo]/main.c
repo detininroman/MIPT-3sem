7bac18a19c33ea93581bdd8707a42aac5e38429a
Roman Detinin <detininroman@gmail.comgit config --global user.name Roman>
2017-09-26 19:22:47 +0300

    fifo v2.0

--- sem3/task2 [fifo]/main.c
+++ sem3/task2 [fifo]/main.c
@@ -8,7 +8,7 @@
 #include <assert.h>
 #include <fcntl.h>
 
-const char* pipe_name = "/tmp/pipe";
+const char* pipe_name = "pipe";
 enum { PIPE_SIZE = 16384 };
 
 int send    (int input);
@@ -41,19 +41,22 @@ int send (int input)
 {
 	char buf [PIPE_SIZE] = {};
 	ssize_t file_size = 0;
+
+	int mkfifo_result = mkfifo (pipe_name, 0777);
+	if (mkfifo_result == -1) return 0;
 	
-	mkfifo (pipe_name, 0666);
+	//printf ("mkfiforesult = %d \n", mkfifo_result);
 
 	int pipe = open (pipe_name, O_WRONLY);
 	if (pipe == -1) {
 		perror ("Can't open the pipe!\n");
 		exit (EXIT_FAILURE);
 	}
-	
+
 	while ((file_size = read (input, buf, PIPE_SIZE)) > 0) {
 		write (pipe, buf, file_size);
 	}
-
+	
 	close (input);
 	close (pipe);
 
@@ -65,17 +68,19 @@ int recieve (int output)
 	char buf [PIPE_SIZE] = {};
 	int file_size = 0;
 
-        int pipe = open (pipe_name, O_RDONLY);
+	int pipe = open (pipe_name, O_RDONLY);
 	if (pipe == -1)  {
 		perror ("Can't open the pipe!\n");
 		exit (EXIT_FAILURE);
 	}
 
-	while ((file_size = read (pipe, buf, PIPE_SIZE)) > 0)
+	while ((file_size = read (pipe, buf, PIPE_SIZE)) > 0) {
 		write (output, buf, file_size);
+	}
 	
 	close (pipe);
 	close (output);
+	unlink (pipe_name);
 
 	return 0;
 }
