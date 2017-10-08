cf88f1b9a51c2291c281d364f19a9b25dad9d1cd
Roman Detinin <detininroman@gmail.comgit config --global user.name Roman>
2017-10-09 00:38:38 +0300

    codestyle changes

--- Queue/getnum.c
+++ Queue/getnum.c
@@ -5,8 +5,8 @@
 
 long get_number (char* arg)
 {
-	char* endptr;
-	long num = strtol (arg, &endptr, 10);
+    char* endptr;
+    long num = strtol (arg, &endptr, 10);
 
     if (endptr == arg) {
         perror ("You did not entered a number!\n");
@@ -23,10 +23,9 @@ long get_number (char* arg)
         exit (EXIT_FAILURE);
     }
 
-	if (*endptr != '\0') {
-		printf("Wrong number!\n");
+    if (*endptr != '\0') {
+        printf("Wrong number!\n");
         exit (EXIT_FAILURE);
     }
-
     return num;
 }
